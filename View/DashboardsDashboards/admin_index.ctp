croogo/croogo

Allow custom sorting of dashboard boxes

Dashboard/View/DashboardBoxes/admin_index.ctp
@@ -1,5 +1,8 @@
 <h2 class="hidden-desktop"><?php echo $title_for_layout; ?></h2>
 <?php
+$this->Croogo->adminScript('Dashboard.admin');
+$this->Html->css('Dashboard.admin', array('inline' => false));
+
 $this->Html
 	->addCrumb('', '/admin', array('icon' => $_icons['home']))
 	->addCrumb(__d('croogo', 'Dashboard'), '/' . $this->request->url);

croogo/dashboards

View/DashboardsDashboards/admin_index.ctp
@@ -1,5 +1,8 @@
 <h2 class="hidden-desktop"><?php echo $title_for_layout; ?></h2>
 <?php
+$this->Croogo->adminScript('Dashboards.admin');
+$this->Html->css('Dashboards.admin', array('inline' => false));
+
 $this->Html
 	->addCrumb('', '/admin', array('icon' => $_icons['home']))
 	->addCrumb(__d('croogo', 'Dashboard'), '/' . $this->request->url);